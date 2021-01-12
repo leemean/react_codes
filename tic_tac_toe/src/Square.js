f1c074536061362658d1a2c3c16c300fb5004854
leemean <leemeany@qq.com>
2021-01-12 11:02:54 +0800

    complete tic_tac_toe program

--- tic_tac_toe/src/Square.js
+++ tic_tac_toe/src/Square.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import './Square.css';
 
-class Square extends React.Component {
-    render(){
-        return (
-        <button className="square" onClick={function(){alert('click')}}>
-            {this.props.value}
+// class Square extends React.Component {
+
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             value: null
+//         }
+//     }
+
+//     render(){
+//         return (
+//         <button className="square" onClick={ ()=> this.props.onClick() }>
+//             {this.props.value}
+//         </button>
+//         );
+//     }
+// }
+
+
+const Square = (props) => {
+    return (
+        <button className="square" onClick={ ()=> props.onClick() }>
+            {props.value}
         </button>
-        );
-    }
+    );
 }
 
 export default Square;
