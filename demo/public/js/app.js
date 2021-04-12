68b0859e5610e22497b6063604ca4dc77672ec6d
leemean <leemeany@qq.com>
2021-04-12 15:32:15 +0800

    commit demo

---
+++ demo/public/js/app.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-param-reassign, operator-assignment */
+
+class Demo extends React.Component {
+
+  render() {
+    return (
+      <div>
+        hello demo!
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Demo />,
+  document.getElementById('content')
+);
